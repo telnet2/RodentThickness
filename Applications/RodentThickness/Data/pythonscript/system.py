974d21dabd7d9d115d127d0f934ad03bf418246d
Joowhi Lee <telnet2@gmail.com>
2014-07-06 11:50:42 -0400

    added append or write

--- Applications/RodentThickness/Data/pythonscript/system.py
+++ Applications/RodentThickness/Data/pythonscript/system.py
@@ -3,8 +3,12 @@ __author__ = 'joohwi'
 import os
 from subprocess import *
 
-def run_process(cmdline):
+def run_process(cmdline, logfile = "", append=False):
     cmd = cmdline.split(" ")
     p = Popen(cmd, stdout=PIPE, shell=False)
     output = p.communicate()
+    if logfile != "":
+        f = open(logfile, "a" if append else "w")
+        f.writelines(output)
+        f.close()
     return output[0]
