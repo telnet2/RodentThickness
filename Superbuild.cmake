5f95998eafcde53213a63b31d3f5afbd3a338429
Beatriz Paniagua <bpaniagua@gmail.com>
2016-05-05 13:21:21 -0400

    BUG: correcting configuration of variables for Qt4

--- Superbuild.cmake
+++ Superbuild.cmake
@@ -16,52 +16,45 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/Common.cmake)
 #If it is build as an extension
 #-----------------------------------------------------------------------------
 
-
-if( RodentThickness_BUILD_SLICER_EXTENSION )
-  
- if(NOT Slicer_SOURCE_DIR)
-    set(EXTENSION_NAME CorticalRodentThicknessAnalysis)
-    set(EXTENSION_HOMEPAGE "https://www.nitrc.org/projects/rodentthickness/")
-    set(EXTENSION_CATEGORY "")
-    set(EXTENSION_CONTRIBUTORS "Marie Jacquemard (UNC)")
-    set(EXTENSION_DESCRIPTION "A tool to measure cortical thickness of rodent brain")
-    set(EXTENSION_STATUS Beta)
-    set(EXTENSION_BUILD_SUBDIRECTORY RodentThickness-build)
+if( ${LOCAL_PROJECT_NAME}_BUILD_SLICER_EXTENSION )
+  if( APPLE )
+    set( CMAKE_EXE_LINKER_FLAGS -Wl,-rpath,@loader_path/../../../../../ )
   endif()
+endif()
 
-  set( USE_SYSTEM_VTK ON )
-  set( USE_SYSTEM_ITK ON)
-  set( USE_SYSTEM_BatchMake OFF)
-  set( USE_SYSTEM_SlicerExecutionModel ON)
-  set( USE_SYSTEM_Rscript OFF)
+#if( RodentThickness_BUILD_SLICER_EXTENSION )
   
-  set(EXTENSION_SUPERBUILD_BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR} )
-  unsetForSlicer(NAMES CMAKE_MODULE_PATH CMAKE_C_COMPILER CMAKE_CXX_COMPILER ITK_DIR SlicerExecutionModel_DIR VTK_DIR QT_QMAKE_EXECUTABLE ITK_VERSION_MAJOR CMAKE_CXX_FLAGS CMAKE_C_FLAGS )
-  find_package(Slicer REQUIRED)
-  include(${Slicer_USE_FILE})
-  unsetAllForSlicerBut( NAMES VTK_DIR QT_QMAKE_EXECUTABLE )
-  resetForSlicer(NAMES CMAKE_MODULE_PATH CMAKE_C_COMPILER CMAKE_CXX_COMPILER ITK_VERSION_MAJOR CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
+# if(NOT Slicer_SOURCE_DIR)
+#    set(EXTENSION_NAME CorticalRodentThicknessAnalysis)
+#    set(EXTENSION_HOMEPAGE "https://www.nitrc.org/projects/rodentthickness/")
+#    set(EXTENSION_CATEGORY "")
+#    set(EXTENSION_CONTRIBUTORS "Marie Jacquemard (UNC)")
+#    set(EXTENSION_DESCRIPTION "A tool to measure cortical thickness of rodent brain")
+#    set(EXTENSION_STATUS Beta)
+#    set(EXTENSION_BUILD_SUBDIRECTORY RodentThickness-build)
+#  endif()
+
+ # set( USE_SYSTEM_VTK ON )
+ # set( USE_SYSTEM_ITK ON)
+ # set( USE_SYSTEM_BatchMake OFF)
+ # set( USE_SYSTEM_SlicerExecutionModel ON)
+ # set( USE_SYSTEM_Rscript OFF)
+  
+ # set(EXTENSION_SUPERBUILD_BINARY_DIR ${${EXTENSION_NAME}_BINARY_DIR} )
+ # unsetForSlicer(NAMES CMAKE_MODULE_PATH CMAKE_C_COMPILER CMAKE_CXX_COMPILER ITK_DIR SlicerExecutionModel_DIR VTK_DIR QT_QMAKE_EXECUTABLE ITK_VERSION_MAJOR CMAKE_CXX_FLAGS CMAKE_C_FLAGS )
+ # find_package(Slicer REQUIRED)
+ # include(${Slicer_USE_FILE})
+ # unsetAllForSlicerBut( NAMES VTK_DIR QT_QMAKE_EXECUTABLE )
+ # resetForSlicer(NAMES CMAKE_MODULE_PATH CMAKE_C_COMPILER CMAKE_CXX_COMPILER ITK_VERSION_MAJOR CMAKE_CXX_FLAGS CMAKE_C_FLAGS)
  
-endif()
-#-----------------------------------------------------------------------------
-# Git protocole option
-#-----------------------------------------------------------------------------
-option(USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
-set(git_protocol "git")
-if(NOT USE_GIT_PROTOCOL)
-  set(git_protocol "http")
-endif()
-
-find_package(Git REQUIRED)
-
-# I don't know who removed the Find_Package for QT, but it needs to be here
-# in order to build VTK if ${LOCAL_PROJECT_NAME}_USE_QT is set.
-#if(${LOCAL_PROJECT_NAME}_USE_QT AND NOT RodentThickness_BUILD_SLICER_EXTENSION)
- #   find_package(Qt4 REQUIRED)
- #   include(${QT_USE_FILE}) 
 #endif()
-find_package(Qt4 REQUIRED)
-include(${QT_USE_FILE}) 
+
+#-----------------------------------------------------------------------------
+# Enable and setup External project global properties
+#-----------------------------------------------------------------------------
+include(ExternalProject)
+include(SlicerMacroEmptyExternalProject)
+include(SlicerMacroCheckExternalProjectDependency)
 
 # Compute -G arg for configuring external projects with the same CMake generator:
 if(CMAKE_EXTRA_GENERATOR)
@@ -83,7 +76,25 @@ else()
   set(cmakeversion_external_update_value 1)
 endif()
 
+#-----------------------------------------------------------------------------
+# Git protocole option
+#-----------------------------------------------------------------------------
+#option(USE_GIT_PROTOCOL "If behind a firewall turn this off to use http instead." ON)
+#set(git_protocol "git")
+#if(NOT USE_GIT_PROTOCOL)
+#  set(git_protocol "http")
+#endif()
 
+#find_package(Git REQUIRED)
+
+# I don't know who removed the Find_Package for QT, but it needs to be here
+# in order to build VTK if ${LOCAL_PROJECT_NAME}_USE_QT is set.
+#if(${LOCAL_PROJECT_NAME}_USE_QT AND NOT RodentThickness_BUILD_SLICER_EXTENSION)
+ #   find_package(Qt4 REQUIRED)
+ #   include(${QT_USE_FILE}) 
+#endif()
+find_package(Qt4 REQUIRED)
+include(${QT_USE_FILE}) 
 
 #-----------------------------------------------------------------------------
 # Superbuild option(s)
@@ -110,16 +121,9 @@ option(USE_SYSTEM_VTK "Build using an externally defined version of VTK" OFF)
 option(USE_SYSTEM_BatchMake "Build using an externally defined version of BatchMake" OFF)
 option(USE_SYSTEM_Rscript "Build using an externally defined version of Rscript" OFF)
 
-#Set(PYTHON_EXECUTABLE "" CACHE FILEPATH "python executable path")
-#Set(PYTHON_LIBRARY "" CACHE FILEPATH "python library ")
-#Set(PYTHON_INCLUDE_DIRS "" CACHE FILEPATH "python include directories ")
 #------------------------------------------------------------------------------
 # ${LOCAL_PROJECT_NAME} dependency list
 #------------------------------------------------------------------------------
-include(ExternalProject)
-include(SlicerMacroEmptyExternalProject)
-include(SlicerMacroCheckExternalProjectDependency)
-
 
 set(ITK_EXTERNAL_NAME ITKv${ITK_VERSION_MAJOR})
 if(WIN32)
@@ -130,10 +134,166 @@ else()
 	set(${LOCAL_PROJECT_NAME}_DEPENDENCIES ${ITK_EXTERNAL_NAME} SlicerExecutionModel VTK BatchMake Rscript)
 endif()
 if(BUILD_STYLE_UTILS)
-  #list(APPEND ${LOCAL_PROJECT_NAME}_DEPENDENCIES Cppcheck KWStyle Uncrustify)
+  list(APPEND ${LOCAL_PROJECT_NAME}_DEPENDENCIES Cppcheck KWStyle Uncrustify)
 endif()
 
-include(${CMAKE_CURRENT_SOURCE_DIR}/SetExternalProjectOptions.cmake)
+#-----------------------------------------------------------------------------
+# Define Superbuild global variables
+#-----------------------------------------------------------------------------
+
+# This variable will contain the list of CMake variable specific to each external project
+# that should passed to ${CMAKE_PROJECT_NAME}.
+# The item of this list should have the following form: <EP_VAR>:<TYPE>
+# where '<EP_VAR>' is an external project variable and TYPE is either BOOL, STRING, PATH or FILEPATH.
+# TODO Variable appended to this list will be automatically exported in ${LOCAL_PROJECT_NAME}Config.cmake,
+# prefix '${LOCAL_PROJECT_NAME}_' will be prepended if it applies.
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS)
+
+# The macro '_expand_external_project_vars' can be used to expand the list of <EP_VAR>.
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS) # List of CMake args to configure BRAINS
+set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES) # List of CMake variable names
+
+# Convenient macro allowing to expand the list of EP_VAR listed in ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+# The expanded arguments will be appended to the list ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS
+# Similarly the name of the EP_VARs will be appended to the list ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES.
+macro(_expand_external_project_vars)
+  set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS "")
+  set(${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES "")
+  foreach(arg ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS})
+    string(REPLACE ":" ";" varname_and_vartype ${arg})
+    set(target_info_list ${target_info_list})
+    list(GET varname_and_vartype 0 _varname)
+    list(GET varname_and_vartype 1 _vartype)
+    list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS -D${_varname}:${_vartype}=${${_varname}})
+    list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES ${_varname})
+  endforeach()
+endmacro()
+
+#-----------------------------------------------------------------------------
+# Common external projects CMake variables
+#-----------------------------------------------------------------------------
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+  MAKECOMMAND:STRING
+  CMAKE_SKIP_RPATH:BOOL
+  CMAKE_BUILD_TYPE:STRING
+  BUILD_SHARED_LIBS:BOOL
+  CMAKE_CXX_COMPILER:PATH
+  CMAKE_CXX_FLAGS_RELEASE:STRING
+  CMAKE_CXX_FLAGS_DEBUG:STRING
+  CMAKE_CXX_FLAGS:STRING
+  CMAKE_C_COMPILER:PATH
+  CMAKE_C_FLAGS_RELEASE:STRING
+  CMAKE_C_FLAGS_DEBUG:STRING
+  CMAKE_C_FLAGS:STRING
+  CMAKE_SHARED_LINKER_FLAGS:STRING
+  CMAKE_EXE_LINKER_FLAGS:STRING
+  CMAKE_MODULE_LINKER_FLAGS:STRING
+  CMAKE_GENERATOR:STRING
+  CMAKE_EXTRA_GENERATOR:STRING
+  CMAKE_INSTALL_PREFIX:PATH
+  CMAKE_LIBRARY_OUTPUT_DIRECTORY:PATH
+  CMAKE_ARCHIVE_OUTPUT_DIRECTORY:PATH
+  CMAKE_RUNTIME_OUTPUT_DIRECTORY:PATH
+  CMAKE_BUNDLE_OUTPUT_DIRECTORY:PATH
+  CTEST_NEW_FORMAT:BOOL
+  MEMORYCHECK_COMMAND_OPTIONS:STRING
+  MEMORYCHECK_COMMAND:PATH
+  SITE:STRING
+  BUILDNAME:STRING
+  ${PROJECT_NAME}_BUILD_DICOM_SUPPORT:BOOL
+  )
+
+if(${LOCAL_PROJECT_NAME}_USE_QT)
+  list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+    ${LOCAL_PROJECT_NAME}_USE_QT:BOOL
+    QT_QMAKE_EXECUTABLE:PATH
+    QT_MOC_EXECUTABLE:PATH
+    QT_UIC_EXECUTABLE:PATH
+    )
+endif()
+
+_expand_external_project_vars()
+set(COMMON_EXTERNAL_PROJECT_ARGS ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+set(extProjName ${LOCAL_PROJECT_NAME})
+set(proj        ${LOCAL_PROJECT_NAME})
+SlicerMacroCheckExternalProjectDependency(${proj})
+
+#-----------------------------------------------------------------------------
+# Set CMake OSX variable to pass down the external project
+#-----------------------------------------------------------------------------
+set(CMAKE_OSX_EXTERNAL_PROJECT_ARGS)
+if(APPLE)
+  list(APPEND CMAKE_OSX_EXTERNAL_PROJECT_ARGS
+    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
+    -DCMAKE_OSX_SYSROOT=${CMAKE_OSX_SYSROOT}
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET})
+endif()
+
+set(${LOCAL_PROJECT_NAME}_CLI_RUNTIME_DESTINATION  bin)
+set(${LOCAL_PROJECT_NAME}_CLI_LIBRARY_DESTINATION  lib)
+set(${LOCAL_PROJECT_NAME}_CLI_ARCHIVE_DESTINATION  lib)
+#if( RodentThickness_BUILD_SLICER_EXTENSION )
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION  ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_RUNTIME_DESTINATION} )
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_LIBRARY_DESTINATION  ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_LIBRARY_DESTINATION} )
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_ARCHIVE_DESTINATION  ${SlicerExecutionModel_DEFAULT_CLI_INSTALL_ARCHIVE_DESTINATION} )
+#else()
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION  bin)
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_LIBRARY_DESTINATION  lib)
+#  set(${LOCAL_PROJECT_NAME}_CLI_INSTALL_ARCHIVE_DESTINATION  lib)
+#endif()
+#-----------------------------------------------------------------------------
+# Add external project CMake args
+#-----------------------------------------------------------------------------
+list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+  BUILD_EXAMPLES:BOOL
+  BUILD_TESTING:BOOL
+  ITK_VERSION_MAJOR:STRING
+  ITK_DIR:PATH
+  VTK_DIR:PATH
+  GenerateCLP_DIR:PATH
+  SlicerExecutionModel_DIR:PATH
+  BatchMake_DIR:PATH
+  INSTALL_RUNTIME_DESTINATION:STRING
+  INSTALL_LIBRARY_DESTINATION:STRING
+  INSTALL_ARCHIVE_DESTINATION:STRING
+  ${LOCAL_PROJECT_NAME}_CLI_LIBRARY_DESTINATION:PATH
+  ${LOCAL_PROJECT_NAME}_CLI_ARCHIVE_DESTINATION:PATH
+  ${LOCAL_PROJECT_NAME}_CLI_RUNTIME_DESTINATION:PATH
+  ${LOCAL_PROJECT_NAME}_CLI_INSTALL_LIBRARY_DESTINATION:PATH
+  ${LOCAL_PROJECT_NAME}_CLI_INSTALL_ARCHIVE_DESTINATION:PATH
+  ${LOCAL_PROJECT_NAME}_CLI_INSTALL_RUNTIME_DESTINATION:PATH
+  )
+
+if( RodentThickness_BUILD_SLICER_EXTENSION )
+  list(APPEND ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARS
+    MIDAS_PACKAGE_API_KEY:STRING
+    MIDAS_PACKAGE_EMAIL:STRING
+    MIDAS_PACKAGE_URL:STRING
+    Slicer_DIR:PATH
+    RodentThickness_BUILD_SLICER_EXTENSION:BOOL
+    )
+endif()
+
+_expand_external_project_vars()
+set(COMMON_EXTERNAL_PROJECT_ARGS ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+
+if(verbose)
+  message("Inner external project args:")
+  foreach(arg ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_ARGS})
+    message("  ${arg}")
+  endforeach()
+endif()
+
+string(REPLACE ";" "^" ${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES "${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES}")
+
+if(verbose)
+  message("Inner external project argnames:")
+  foreach(argname ${${CMAKE_PROJECT_NAME}_SUPERBUILD_EP_VARNAMES})
+    message("  ${argname}")
+  endforeach()
+endif()
+
+
 
 #------------------------------------------------------------------------------
 # Configure and build
@@ -155,5 +315,14 @@ ExternalProject_Add(${proj}
     -D${PYTHON_INCLUDE_DIRS}:PATH=${PYTHON_INCLUDE_DIRS}
     -Dpathexecpython:PATH=${pathexecpython}
   INSTALL_COMMAND ""
+  )
+
+## Force rebuilding of the main subproject every time building from super structure
+ExternalProject_Add_Step(${proj} forcebuild
+    COMMAND ${CMAKE_COMMAND} -E remove
+    ${CMAKE_CURRENT_BUILD_DIR}/${proj}-prefix/src/${proj}-stamp/${proj}-build
+    DEPENDEES configure
+    DEPENDERS build
+    ALWAYS 1
   )
 
